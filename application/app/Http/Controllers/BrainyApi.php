ca10f946c368fe965c9c2b2f8eb452ca1e53fc46
Konstantin <Satana.Konst@gmail.com>
2023-06-29 13:12:14 +0300

    Добавлена ссылка на доку по API

--- application/app/Http/Controllers/BrainyApi.php
+++ application/app/Http/Controllers/BrainyApi.php
@@ -6,6 +6,9 @@ use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Mockery\Exception;
 
+/**
+ * https://docs.brainycp.com/index.php/API:_%D1%83%D0%BF%D1%80%D0%B0%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5_%D0%B2%D0%B5%D0%B1-%D1%81%D0%B5%D1%80%D0%B2%D0%B5%D1%80%D0%BE%D0%BC
+ */
 class BrainyApi extends Controller
 {
     private $panelUrl = null;
